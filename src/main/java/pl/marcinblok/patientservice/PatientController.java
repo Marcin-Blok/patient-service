8d71d6600aa35af12c688afc6804e4bd3e077797
Marcin-Blok <marcinblok7980@gmail.com>
2020-08-14 07:59:41 +0200

    Dodanie sprawdzenia czy imie i nazwisko jest różne od null-em w metodzie POST

--- src/main/java/pl/marcinblok/patientservice/PatientController.java
+++ src/main/java/pl/marcinblok/patientservice/PatientController.java
@@ -20,26 +20,27 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @PostMapping(path = "/patients")
-    public @ResponseBody ResponseEntity<String> add(@RequestBody Patient patient) {
+    public @ResponseBody
+    ResponseEntity<String> add(@RequestBody Patient patient) {
         String pesel = patient.getPesel();
         Matcher matcher = pattern.matcher(pesel);
 
-        if(patient.getPesel() != null){
+        if (patient.getName() != null && patient.getSurname() != null && patient.getPesel() != null) {
             if (matcher.matches()) {
                 try {
                     patientRepository.save(patient);
-                    return new ResponseEntity<>("Zapisano",HttpStatus.CREATED);
+                    return new ResponseEntity<>("Zapisano", HttpStatus.CREATED);
                 } catch (IllegalArgumentException e) {
                     return new ResponseEntity<>("Nie udało się dodać pacjenta", HttpStatus.NOT_MODIFIED);
                 }
             } else {
                 return new ResponseEntity<>("Nie zapisano, długość numeru pesel jest niepoprawna, bądź użyto niedozwolonych znaków lub liter", HttpStatus.NOT_ACCEPTABLE);
             }
-        }else{
-            return new ResponseEntity<>("Nie zapisano, podaj pesel", HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            return new ResponseEntity<>("Nie zapisano, następujące pola muszą zostać wypełnione: Imię, Nazwisko, Pesel", HttpStatus.NOT_ACCEPTABLE);
         }
     }
-    
+
 
     @GetMapping(path = "/patients")
     public @ResponseBody
@@ -54,7 +55,7 @@ public class PatientController {
 
     @DeleteMapping(path = "/patients/{id}")
     public ResponseEntity<Void> deletePatientById(@PathVariable Integer id) {
-          patientRepository.deleteById(id);
+        patientRepository.deleteById(id);
         return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 
